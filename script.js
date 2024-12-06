e6533f9dde30232ac24225f8973ee03871354040
THAVARAK CHHOAN <thavarajchoan@gmail.com>
2024-12-06 15:39:36 +0700

    r-p

---
+++ script.js
@@ -0,0 +1,7 @@
+<>
+  <script
+    src="https://kit.fontawesome.com/12e2066573.js"
+    crossorigin="anonymous"
+  ></script>
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+</>;
